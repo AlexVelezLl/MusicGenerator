18ab52b40d73f1bd40608d4360702435d2b79579
JFNebel <jfnebeldunn@gmail.com>
2022-01-06 16:53:43 -0500

    midi_input_encoder.py can now transpose MIDI input if generated using notation software (e.g. MuseScore 3)

--- AIModel/midi_input_encoder.py
+++ AIModel/midi_input_encoder.py
@@ -1,24 +1,25 @@
 from shared import check_durations
 import music21.stream
 import music21.converter
+import music21.pitch
+import music21.interval
 
 
-# PATH CONSTANTS
-MIDI_INPUT_PATH = 'input/test_melody.mid'
+# USER INPUTS
+MIDI_INPUT_PATH = 'inputs/test_melody_Emaj.mid'
+KEY = 'E'       # 12 Notes
+MODE = 'major'  # major or minor
 
 
 
 def encode_midi_input():
 
-
     # Load the MIDI melody
-    input_seed = music21.converter.parse(MIDI_INPUT_PATH) 
-
+    raw_input_midi_seed = music21.converter.parse(MIDI_INPUT_PATH) 
 
 
     # Check for non-supported note/rest durations
-    seed_is_duration_complaint = check_durations(input_seed)
-    print(seed_is_duration_complaint)
+    seed_is_duration_complaint = check_durations(raw_input_midi_seed)
     if(not seed_is_duration_complaint):
         # TODO: Mandar un mensaje de error que diga que no mande notes/rest con fracciones menor a 16th (fusas, semi-fusas, semi-corcheas con punto y silencios respectivos)
         pass
@@ -26,11 +27,16 @@ def encode_midi_input():
     
 
     # Transpose the melody
+    interval = music21.interval.Interval(music21.pitch.Pitch(KEY), music21.pitch.Pitch('C'))
+    transposed_seed = raw_input_midi_seed.transpose(interval)
+    transposed_seed.show()
+
+
 
     # Encode in time series string
 
 
-    return input_seed
+    return transposed_seed
 
 
 
@@ -40,9 +46,25 @@ if __name__ == "__main__":
 
 
 
+    # Metadata key test
+    # for element in input_seed.getElementsByClass(music21.stream.Stream):
+    #     print(element.getElementsByClass(music21.stream.Measure)[0][4])
+
+
+
+    
+
+
+
 
 
 
 # TODO:
-# 1. Arregla el check_durations
-# 1. Cómo hago un código que pase el key por consola.
+# - Probar si Music21 es capaz de redondear correctamente valores cuando Arturo toca a pelo o necesitan ser duraciones grideadas.
+# - Mira si existe alguna armadura en metadata cuando Arturo toca una melodía MIDI en un keyb.
+# - Si sí existen armaduras en MIDI keyb: Mira si el problema de las armaduras se mantiene al transponer (ergo, la armadura del MIDI metadata no coincide con la verdadera armadura)
+# - Es muy posible que Cmaj sea un defecto, vas a tener que hacer que el script pueda setear el key a cmaj (quiero poder .show() y que la armadura sea Cmaj, cuando ya esté transpuesto para evitar problemas futuros)
+# - Termina el encoding step.
+# - Consultar qué hacer con el mensaje en caso de que las duraciones no sean aceptables.
+# - Posiblemente hay código reutilizable en el proceso de transposición entre preprocessing y encoding.
+# - Refactor input constants so that it connects with GUI.
