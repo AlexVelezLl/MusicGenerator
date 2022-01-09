8d8d4841547782915ec50668427fb71f5a8cf465
JFNebel <jfnebeldunn@gmail.com>
2022-01-08 22:31:49 -0500

    First version of multi-use music transposition wrapper in utils.py.

--- AIModel/training_data_preprocessing.py
+++ AIModel/training_data_preprocessing.py
@@ -1,6 +1,7 @@
-from constants import RAW_DATASET_PATH
 import os
 import music21.converter
+from constants import RAW_DATASET_PATH
+from utils import check_durations
 
 
 
@@ -18,6 +19,42 @@ def preprocess_data():
                 scores.append(score)
 
 
+
+
+    # Filter scores with unsupported durations
+    for score in scores:
+
+        score_is_duration_complaint = check_durations(score)
+        if not score_is_duration_complaint: scores.remove(score)
+
+
+
+
+
+    # Transpose scores to Cmaj/Amin keys
+
+
+    # Encode songs in time-series representation
+
+    # Create look-up table as vocabulary
+
+
+
+
+
 if __name__ == '__main__':
     preprocess_data()
+
+
+
+
+
+
+
+
+# TESTS, TODO: Delete this test
+
+# Check filtering due to durations:
+# scores[0].flat.notesAndRests[7].duration.quarterLength = 0.125
+# print(scores[0].flat.notesAndRests[7].duration.quarterLength)
 
--- AIModel/utils.py
+++ AIModel/utils.py
@@ -1,10 +1,59 @@
 from constants import SUPPORTED_DURATIONS
+import music21.interval
+import music21.pitch
+import music21.stream
+import music21.key
 
 
-def check_durations(song):
+def check_durations(score):
 
-    for musical_event in song.flat.notesAndRests:
+    for musical_event in score.flat.notesAndRests:
         if musical_event.duration.quarterLength not in SUPPORTED_DURATIONS: return False
 
     return True
+
+
+
+
+
+
+def transpose_music_wrapper(score, key='n/a', mode='n/a'):
+    
+    # TODO: Validaciones al user input key y mode (manda basura)
+    
+
+    # No user input (training)
+    if key == 'n/a':
+    
+        # Get key by metadata
+        parts = score.getElementsByClass(music21.stream.Part)
+        measures_part0 = parts[0].getElementsByClass(music21.stream.Measure) 
+        key = measures_part0[0][4] # Here is where key resides if in metadata
+
+        # If that fails, estimate key
+        if not isinstance(key, music21.key.Key):
+            key = score.analyze("key") 
+
+        # Get mode
+        mode = key.mode
+
+
+    # Get interval
+    if mode=='major':
+        interval = music21.interval.Interval(music21.pitch.Pitch(key.tonic.step), music21.pitch.Pitch('C'))  
+    elif mode=='minor': # It's minor
+        interval = music21.interval.Interval(music21.pitch.Pitch(key.tonic.step), music21.pitch.Pitch('A'))  
+    else:
+        interval = music21.interval.Interval("P1") 
+
+
+    # Transpose
+    transposed_score = score.transpose(interval)
+    
+    return transposed_score
+
+
+
+
+
 
